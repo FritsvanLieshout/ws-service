fb2e2cda8a3fcffb383412ec4ca9b6c59a553e4c
Frits van Lieshout <fritske05@gmail.com>
2021-05-24 12:25:25 +0200

    Initial commit

---
+++ src/test/java/com/kwetter/frits/wsservice/WsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kwetter.frits.wsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
